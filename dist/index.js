99f6bacc36b1ecbef119a63882442ea47f10ecb6
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-04-05 14:37:38 +0300

    update

--- dist/index.js
+++ dist/index.js
@@ -1,82 +1,64 @@
-interface Spot {
-    isAvailable: boolean,
-    spot: Vehicle | null,
-    vehicleId: string,
-}
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 class Garage {
-    #spot: Spot;
-
+    #spot;
     constructor() {
         this.#spot = {
             isAvailable: true,
             spot: null,
             vehicleId: "noID",
-        }
+        };
     }
-
-    parkVehicle(vehicle: Vehicle): Vehicle | void {
-        if(this.#spot.isAvailable) {
+    parkVehicle(vehicle) {
+        if (this.#spot.isAvailable) {
             this.#spot.isAvailable = false;
             this.#spot.spot = vehicle;
             this.#spot.vehicleId = vehicle.getVehicleId();
-            console.log(`${vehicle} is parked.`)
+            console.log(`${vehicle} is parked.`);
             return vehicle;
-        } else {
+        }
+        else {
             console.log('No available spot.');
             return;
         }
     }
-
-    unparkVehicle(vehicle: Vehicle): Vehicle | void {
-        if(this.#spot.vehicleId === vehicle.getVehicleId()) {
+    unparkVehicle(vehicle) {
+        if (this.#spot.vehicleId === vehicle.getVehicleId()) {
             this.#spot.isAvailable = true;
             this.#spot.spot = null;
             console.log(`${vehicle} is unparked.`);
-            return vehicle; 
-        } else {
+            return vehicle;
+        }
+        else {
             console.log('Un-authorized user.');
             return;
         }
     }
-
-}   
-
+}
 class Vehicle {
-    #id: string;
-    #wheels: number;
-    
-    constructor(wheels: number) {
+    #id;
+    #wheels;
+    constructor(wheels) {
         this.#wheels = wheels;
         this.#id = crypto.randomUUID();
     }
-
-    getVehicleId(): string {
+    getVehicleId() {
         return this.#id;
     }
 }
-
-class Bicycle extends Vehicle{
-    #type: 'MOUNTAIN' | 'ROAD';
-
-    constructor(type: 'MOUNTAIN' | 'ROAD') {
+class Bicycle extends Vehicle {
+    #type;
+    constructor(type) {
         super(2);
         this.#type = type;
     }
 }
-
 const newGarage = new Garage();
 const newBike = new Bicycle('MOUNTAIN');
-
 newGarage.parkVehicle(newBike);
-
 const newBike2 = new Bicycle('ROAD');
-
 newGarage.parkVehicle(newBike2);
 newGarage.unparkVehicle(newBike2);
-
 newGarage.unparkVehicle(newBike);
-
 newGarage.parkVehicle(newBike2);
-
-
+//# sourceMappingURL=index.js.map
